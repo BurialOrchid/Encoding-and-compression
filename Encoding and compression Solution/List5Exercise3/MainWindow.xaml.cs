bba9426a1af4e5f967448bda26469311fa02632c
Krzysztof <krzysiek.mroziewicz@gmail.com>
2020-06-22 20:21:05 +0200

    Updated Readme file Finished List 5 Exercise 3

--- Encoding and compression Solution/List5Exercise3/MainWindow.xaml.cs
+++ Encoding and compression Solution/List5Exercise3/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace List5Exercise3
 {
@@ -26,18 +16,13 @@ namespace List5Exercise3
     public partial class MainWindow : Window
     {
         private byte[] pictureBytes;
+        private byte[] differenceBytes;
+        private int pictureHeight;
+        private int pictureWidth;
 
         public MainWindow()
         {
             InitializeComponent();
-            LoadFile();
-        }
-
-        private void LoadFile()
-        {
-            string filepath = "../../../Flower_640x500_8bit_Grayscale.raw";
-            pictureBytes = File.ReadAllBytes(filepath);
-            ImagePanel.Source = LoadImage(640, 500, pictureBytes);
         }
 
         private BitmapSource LoadImage(int width, int height, byte[] imageData)
@@ -46,6 +31,126 @@ namespace List5Exercise3
             var stride = (width * format.BitsPerPixel + 7) / 8;
 
             return BitmapSource.Create(width, height, 96, 96, format, null, imageData, stride);
+        }
+
+        private void LoadFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select RAW FILE",
+                Filter = "RAW files|*.raw"
+            };
+            if (openFileDialog.ShowDialog() == true)
+                pictureBytes = File.ReadAllBytes(openFileDialog.FileName);
+            FileNameValueLbl.Content = openFileDialog.SafeFileName;
+
+            differenceBytes = CalculateDifferenceBytes(pictureBytes);
+        }
+
+        private void SizeConfBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Int32.TryParse(HeightTxtBox.Text, out pictureHeight))
+            {
+                HeightTxtBox.Foreground = Brushes.Red;
+            }
+            if (!Int32.TryParse(WidthTxtBox.Text, out pictureWidth))
+            {
+                WidthTxtBox.Foreground = Brushes.Red;
+            }
+
+            SizeValueLbl.Content = $"{pictureWidth}x{pictureHeight}";
+        }
+
+        private void TxtBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.Foreground = Brushes.Black;
+        }
+
+        private void ShowPictureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (pictureHeight <= 0 || pictureWidth <= 0)
+            {
+                MessageBox.Show("Height or width isn't set", "List 5 Exercise 3 Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (pictureBytes == null)
+            {
+                MessageBox.Show("Picture not Selected", "List 5 Exercise 3 Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else { ImagePanel.Source = LoadImage(pictureWidth, pictureHeight, pictureBytes); }
+        }
+
+        private void DifferenceImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (pictureHeight <= 0 || pictureWidth <= 0)
+            {
+                MessageBox.Show("Height or width isn't set", "List 5 Exercise 3 Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (differenceBytes == null)
+            {
+                MessageBox.Show("Picture not Selected", "List 5 Exercise 3 Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                ImagePanel.Source = LoadImage(pictureWidth, pictureHeight, differenceBytes);
+            }
+        }
+
+        private byte[] CalculateDifferenceBytes(byte[] picture)
+        {
+            byte[] DifferenceTable = new byte[picture.Length];
+            DifferenceTable[0] = picture[0];
+            for (int i = 1; i < picture.Length; i++)
+            {
+                int difference = (int)picture[i] - (int)picture[i - 1];
+                if (difference < 0) { difference += 256; }
+                DifferenceTable[i] = Convert.ToByte(difference);
+            }
+            return DifferenceTable;
+        }
+
+        private byte[] CalculateOriginalBytes(byte[] difference)
+        {
+            byte[] OriginalTable = new byte[difference.Length];
+            OriginalTable[0] = difference[0];
+            for (int i = 1; i < difference.Length; i++)
+            {
+                int pixel = (int)OriginalTable[i - 1] + (int)difference[i];
+                if (pixel > 255) { pixel -= 256; }
+                OriginalTable[i] = Convert.ToByte(pixel);
+            }
+            return OriginalTable;
+        }
+
+        private void OriginalImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (pictureHeight <= 0 || pictureWidth <= 0)
+            {
+                MessageBox.Show("Height or width isn't set", "List 5 Exercise 3 Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (differenceBytes == null)
+            {
+                MessageBox.Show("Picture not Selected", "List 5 Exercise 3 Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                byte[] originalImage = CalculateOriginalBytes(differenceBytes);
+                ImagePanel.Source = LoadImage(pictureWidth, pictureHeight, originalImage);
+            }
+        }
+
+        private void SaveDifferenceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "RAW files|*.raw",
+                Title = "Save Difference Table"
+            };
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                File.WriteAllBytes(saveFileDialog.FileName, differenceBytes);
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -4,5 +4,90 @@ Repo for coding exercises for encoding and compression subject.
 Sixth semester of classes at the university. 
 Uniwersytet Opolski 2020
 
-List 1 Exercise 9
-Write a program that creates a probabilistic model for a text file counting the number of occurrences of individual letters.
+Language of choice C#
+
+Most of exercises done with .Net Core console projects
+Exercises form list 5 done with .Net Core WPF App
+**.Net Core version 3.1**
+
+# Exercises to do 
+### * List 1 Exercise 9
+Write a program that creates a probabilistic model for a text file counting
+the number of occurrences of individual letters.
+
+### * List 1 Exercise 10
+Write a program that calculates entropy for a file in a probabilistic model with
+previous task.
+
+### * List 2 Exercise 4
+Write a program that selects letters from the Polish alphabet and creates 200 four-letter words from them in terms of the way:
+a) random selection with equal probabilities,
+b) on the basis of the attached file 4wyrazy.txt u download the probabilistic model and
+	generate words based on this model,
+(extra)c) repeat point (b) applies a uniform context for the second, third and fourth
+	letters and probabilistic model for the first letter,
+(extra)d) repeat point (b) use the two-letter context for the third and fourth letters,
+	single letter context for the second letter and probabilistic model for the first
+	letters.
+
+### * List 3 Exercise 5 (extra)
+Write the program:
+a) finding the Huffman code for the symbols appearing in the selected file
+	and calculating the average length of this code and comparing it to entropy
+	(code redundancy), check for differences in distribution
+	probabilities in relation to the distribution taken into account at
+	Code constructions affect file compression (bit average)
+b) compressing and decompressing any file using encoding
+	Huffman (used to compress and decompress Huffman code can
+	save in another file),
+c) compressing and decompressing any file using dynamic
+	Huffman coding
+
+### * List 4 Exercise 4
+Write the program:
+a) finding a marker from the range [0, 1) for any short sequence
+	ASCII and decoding this tag,
+(extra)b) implementing any variation of arithmetic coding
+	generating binary code and decoding incrementally
+	generated code.
+
+### * List 5 Exercise 3
+Write a program that:
+a) displays a picture saved in the file:
+	"Flower_640x500_8bit_Grayscale.raw" in .raw format, i.e. as a string
+	bytes, where each byte is the shade of gray of the next pixel in
+	image line by line (image size W x H: 640 x 500),
+b) sets the table of differences (prediction) between neighboring pixels of the image
+	from point a.,
+c) display image from pixel difference table (write program so that the alphabet for
+	the calculated differences also had 256 symbols),
+d) restores the original image from the difference table.
+e) encode original image and prediction image with Huffman encoder and
+	compare the compression ratio.
+
+### * List 5 Exercise 4 (extra)
+Write a program that creates tables of differences (separately for R, G and B) for the image
+colored and encodes the processed image with Huffman encoder. Compare the degree
+compression of the original image and the image processed by calculation
+differences. Recreate the original image from the difference tables.
+
+### * List 5 Exercise 5 (extra)
+Instead of a simple sequence of differences, use another selected prediction scheme with
+JPEG-LS encoding and use in task 3 or 4.
+
+### * List 6 Exercise 2
+Write a program that uses the digram encoding algorithm for
+encode text files.
+(extra) Decode text files. Analyze the impact of size
+dictionary and size of compressed files per degree of compression. Apply
+encoder for files containing other text than the one it stayed for
+created dictionary - how it affected the obtained compression.
+
+### * List 6 Exercise 3 (extra)
+Write a program that displays the image in progressive transmission e.g. by clicking
+the next image approximation is displayed in the button. Experiment with
+various ways to generate image approximation in transmission
+progressive (you can, for example, calculate the average for pixels within a block).
+
+Instead of a simple sequence of differences, use another selected prediction scheme with
+JPEG-LS encoding and use in task 3 or 4.
